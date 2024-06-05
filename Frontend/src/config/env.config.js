85828c3b53cabaa24bd0a5937fc08d6d2f4352b7
Prakash kumar singh <Pk87096639@gmail.com>
2024-06-05 19:12:47 +0530

    Created

--- Frontend/src/config/env.config.js
+++ Frontend/src/config/env.config.js
@@ -1,5 +1,5 @@
 export const config = {
   // VITE_BACKEND: "http://localhost:4000/hubx/api/v1/user",
   VITE_BACKEND:
-    "https://redux-image-views-828fqnjuq-prakashs-projects-29909feb.vercel.app/hubx/api/v1/user",
+    "https://redux-image-views-git-main-prakashs-projects-29909feb.vercel.app/hubx/api/v1/user",
 };
