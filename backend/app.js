0cf8583325d96e7b5237306811c97721dfe79d26
Prakash kumar singh <Pk87096639@gmail.com>
2024-06-05 18:58:44 +0530

    Created

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.use(
   cors({
     // origin: "http://localhost:5173",
     origin:
-      "https://redux-image-view-git-main-prakashs-projects-29909feb.vercel.app",
+      "https://redux-image-view-kj7a82clo-prakashs-projects-29909feb.vercel.app",
     credentials: true,
   })
 );
